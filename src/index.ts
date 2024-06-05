21887a88a187353f5ec5e8b770a47c5b29653d82
pflanagan <pflanagan@grubhub.com>
2024-06-05 11:01:18 -0400

    setup environment

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
 import { GuitarVisual } from "canvas/GuitarVisual";
 
-const canvasElement = document.getElementById('canvas') as HTMLCanvasElement;
+function main() {
+  const canvasElement = document.getElementById('canvas') as HTMLCanvasElement;
 
-if (!canvasElement) {
-  throw "Unable to get canvas context";
+  if (!canvasElement) {
+    throw "Unable to get canvas context";
+  }
+
+  const context = canvasElement.getContext("2d");
+  canvasElement.width = window.innerWidth;
+  canvasElement.height = window.innerHeight;
+
+  if (!context) {
+    throw "Unable to get canvas context";
+  }
+
+  const visual = new GuitarVisual(context);
+  visual.setup();
+  visual.start();
 }
 
-
-const context = canvasElement.getContext("2d");
-canvasElement.width = window.innerWidth;
-canvasElement.height = window.innerHeight;
-
-if (!context) {
-  throw "Unable to get canvas context";
-}
-
-const visual = new GuitarVisual(context);
-visual.setup();
-visual.start();
+main();
